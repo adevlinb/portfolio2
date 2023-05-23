52e08ac48f77798e7c80691f7701fb8ee2b8bd20
adevlinb <adevlin.booth@gmail.com>
2023-05-23 15:45:47 -0700

    updates article page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="w-full bored-t-2 border-solid border-dark font-medium text-lg">
         <Layout className="py-8 flex items-center justify-between">
-            <span>{new Date().getFullYear} &copy; All Rights Reserved</span>
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
             <div className="flex items-center">
                 Built With <span className="text-primary text-2x1 px-1">&hearts;</span>
                 By&nbsp;<Link href="https://xyla-books.herokuapp.com/" target="_blank" className="underline underline-offset-2">ADevlinB</Link>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,14 +5,20 @@ import Link from "next/link";
 import Image from "next/image";
 import { GithubIcon } from "@/components/Icons";
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+import { motion } from "framer-motion";
+
+const FramerImage = motion(Image)
 
 const FeaturedProject = ({ type, title, summary, image, link, github, project }) => {
     return (
         <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl">
-            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark"/>
+            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl "/>
             <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg">
-                <Image src={image} alt={title} className="w-full h-auto"/>
-            </Link>
+                <FramerImage src={image} alt={title} className="w-full h-auto"
+                    whileHover={{scale:1.05}}
+                    transition={{duration:0.2}}
+                />
+            </Link> 
             <div className="w-1/2 flex flex-col items-start justify-between p-6">
                 <span className="text-primary font-medium text-xl">{type}</span>
                 <Link href={link} target="_blank" className="hover:underline underline-offset-2">
@@ -31,8 +37,12 @@ const FeaturedProject = ({ type, title, summary, image, link, github, project })
 const Project = ({ type, title, image, link, github, project }) => {
     return (
         <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light shadow-2xl p-6 relative">
+            <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl "/>
             <Link href={link} target="_blank" className="w-full cursor-pointer overflow-hidden rounded-lg">
-                <Image src={image} alt={title} className="w-full h-auto"/>
+                <FramerImage src={image} alt={title} className="w-full h-auto"
+                    whileHover={{scale:1.05}}
+                    transition={{duration:0.2}}
+                />
             </Link>
             <div className="w-full flex flex-col items-start justify-between mt-4">
                 <span className="text-primary font-medium text-xl">{type}</span>
@@ -59,7 +69,7 @@ export default function projects() {
         <main className="w-full mb-16 flex flex-col items-center justify-center">
             <Layout className="pt-16">
                 <AnimatedText text="Imagination Trumps Knowledge!" className="mb-16"/>
-                <div className="grid grid-cols-12 gap-12">
+                <div className="grid grid-cols-12 gap-12 gap-y-32">
                     <div className="col-span-12">
                         <FeaturedProject
                             title="Crypto Screener Application"
@@ -95,7 +105,6 @@ export default function projects() {
                     <div className="col-span-6">
                         <Project title="Crypto Screener Application" image={project1} link="/" github="/" type="Featured Project" project="google.com"/>
                     </div>
-
                 </div>
             </Layout>
         </main>

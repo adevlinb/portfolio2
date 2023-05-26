b60716f1860ae0b65f870b0cb1f514f4ca488072
adevlinb <adevlin.booth@gmail.com>
2023-05-25 18:15:38 -0700

    updates about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import Layout from "./Layout";
 
 export default function Footer() {
   return (
-    <footer className="w-full bored-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light">
-        <Layout className="py-8 flex items-center justify-between">
+    <footer className="w-full bored-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-t dark:border-light sm:text-base">
+        <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
             <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
-            <div className="flex items-center">
+            <div className="flex items-center lg:py-2">
                 Built With <span className="text-primary dark:text-primaryDark text-2x1 px-1">&hearts;</span>
                 By&nbsp;<Link href="https://xyla-books.herokuapp.com/" target="_blank" className="underline underline-offset-2">ADevlinB</Link>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
                 <AnimatedText 
                 	text="Turning Vision Into Reality With Code And Design." 
 					className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"/>
-                <p className='my-4 text-base font-medium'>
+                <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
 					As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
 					Explore my latest projects and articles, showcasing my expertise in React.js and web development.
                 </p>
@@ -35,11 +35,11 @@ export default function Home() {
 						className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg border-solid 
 						border-light border-2 text-lg font-semibold hover:bg-light hover:text-dark 
 						hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark 
-						hover:dark:text-light hover:dark:border-light'
+						hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base'
 						download={true}>
 						Resume <LinkArrow className={"w-6 ml-1"}/>
 					</Link>
-					<Link href="mailto:adevlin.booth@gmail.com" className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light'>Contact</Link>
+					<Link href="mailto:adevlin.booth@gmail.com" className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'>Contact</Link>
                 </div>
             </div>
         </div>

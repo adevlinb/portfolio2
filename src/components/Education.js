324a5a493edd9812689e289456a6441b8cdd5c4d
adevlinb <adevlin.booth@gmail.com>
2023-05-26 12:46:59 -0700

    completes responsive about page

--- src/components/Education.js
+++ src/components/Education.js
@@ -10,12 +10,12 @@ const Details = ({ type, time, place, info }) => {
     })
 
     return (
-        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]'>
             <LiIcon reference={ref} scrollYProgress={scrollYProgress}/>
             <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}}>
-                <h3 className='capitalize font-bold text-2xl'>{type}</h3>
-                <span className='capitalize font-medium text-dark/75 dark:text-light/75'>{time} | {place}</span>
-                <p className='font-medium w-full'>{info}</p>
+                <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{type}</h3>
+                <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>{time} | {place}</span>
+                <p className='font-medium w-full md:text-sm'>{info}</p>
             </motion.div>
         </li>
     )
@@ -30,12 +30,10 @@ export default function Education() {
 
     return (
         <div className='my-64'>
-            <h2 className='font-bold text-8xl mb-32 w-full text-center'>
-                Education
-            </h2>
-            <div className='w-[75%] mx-auto relative'>
-                <motion.div style={{scaleY: scrollYProgress}} ref={ref} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light'/>
-                <ul className='w-full flex flex-col items-start justify-between ml-4'>
+            <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>Education</h2>
+            <div className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
+                <motion.div style={{scaleY: scrollYProgress}} ref={ref} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[29px] xs:left-[20px]'/>
+                <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
                     <Details type="Bachelor Of Science In Computer Science" time="2016-2020" place="Massachusetts Institute Of Technology (MIT)" info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial Intelligence." />
                     <Details type="Master Of Computer Science" time="2020-2022" place="Stanford University" info="Completed a master's project on deep learning, developing a new neural network architecture for natural language understanding." />
                     <Details type="Online Coursework" time="2016-2020" place="Coursera And EdX" info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine Learning Engineering." />
